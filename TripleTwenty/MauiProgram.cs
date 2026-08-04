9ad735346c019d17e234cf01d5265312925bc3ab
Christiaan van Attekum <d.v.attekum@gmail.com>
2026-08-04 15:04:39 +0200

    Created a style within the app itself.

--- TripleTwenty/MauiProgram.cs
+++ TripleTwenty/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using Plugin.LocalNotification;
 using TripleTwenty.ViewModels;
 using TripleTwenty.Views;
@@ -12,6 +13,7 @@ namespace TripleTwenty
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .UseLocalNotification()
                 .ConfigureFonts(fonts =>
                 {
